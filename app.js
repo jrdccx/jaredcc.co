d66d35f473294bf20990db6dddba054ca6469739
Ev Bogue <ev@evbogue.com>
2013-04-15 13:46:59 -0400

    redirect all non-existant routes to index

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -30,6 +31,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
+app.get('*', routes.index);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
