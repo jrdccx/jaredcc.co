1afdd111a89b9fdf8b6eb5faf3b1769dda3df051
JaredCC <jrdccx@gmail.com>
2013-05-10 00:46:53 -0600

    comment out Writing route | update shorcut img

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ exports.index = function(req, res){
   res.render('index', { title: 'Jaredcc.co | Storyteller with an emphasis in writing' });
 };
 
+/*
 exports.writing = function(req, res){
   res.render('writing', { title: 'Writing | Jared Chapman'});
 };
+*/
