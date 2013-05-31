5a745248b0bc7b748cae17bb5dbdd0b59c490db6
JaredCC <jrdccx@gmail.com>
2013-05-31 12:24:50 -0700

    Simplify jaredcc.com

--- proxy-table.js
+++ proxy-table.js
@@ -5,7 +5,7 @@ var util = require('util')
 httpProxy.createServer({
     router: {
         'localhost': 'localhost:8080',
-        'www.yourdomain.com': 'localhost:7777'
+        'www.jaredcc.com': 'localhost:7777'
     }
 }).listen(80);
 
